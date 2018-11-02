b36674d8e37a6ae2e5ab09574f4cbfb340cdc43c
Emmanuel Harel <emmanuel.harel@zenika.com>
2018-11-02 23:10:07 +0100

    Premier commit

---
+++ src/main/java/legacy/Item.java
@@ -0,0 +1,43 @@
+package legacy;
+
+public class Item {
+
+    private String name;
+    private int sellIn;
+    private int quality;
+
+    public Item(String name, int sellIn, int quality) {
+        this.name = name;
+        this.quality = quality;
+        this.sellIn = sellIn;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getSellIn() {
+        return sellIn;
+    }
+
+    public int getQuality() {
+        return quality;
+    }
+
+    public void setSellIn(int sellIn) {
+        this.sellIn = sellIn;
+    }
+
+    public void setQuality(int quality) {
+        this.quality = quality;
+    }
+
+    @Override
+    public String toString() {
+        return "Item{" +
+                "name='" + name + '\'' +
+                ", sellIn=" + sellIn +
+                ", quality=" + quality +
+                '}';
+    }
+}
